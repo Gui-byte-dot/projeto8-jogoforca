e8ac309362a107953d3f59aa66d224147767d5f7
guilherme <grgomes82@gmail.com>
2022-09-22 00:59:12 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 
 export default function App(props){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const [valor, setValor] = React.useState(false);
-    const mostrar = () => setValor(true);
-    
+    // const [valor, setValor] = React.useState(false);
+    // const mostrar = () => setValor(true);
+    const [valor, setValor] = React.useState([]);
+    //    const task = [];
+    // function mostrar(index){
+    //     console.log(alfabeto[index])
+    // }
+
+function mostrar(index){
+    let novoArray = [...valor, alfabeto[index]];
+    setValor(novoArray);
+    console.log(novoArray)
+    console.log(novoArray[novoArray.length -1].includes('a'))
+}
     return(
       
         <>
@@ -12,13 +23,15 @@ export default function App(props){
                 <img src="forca0.png" alt="forca" />
             </div>
             <div class="letras">
-                {alfabeto.map((letra,index) => (<button key={index} onClick={mostrar}>{letra}</button>))}
+                {alfabeto.map((letra,index) => (<button key={index} onClick={() => mostrar(index)}>{letra}</button>))}
             </div>
             <div>
-                {valor ? <p>{alfabeto[props.index]}</p> : null}
             </div>
             
         </>
     )
 }
 
+// {valor ? <p>{alfabeto[props.index]}</p> : null}
+// () => console.log(alfabeto[index])
+// () => setValor(alfabeto[index])
